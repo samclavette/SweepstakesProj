080b5cc644ff7a4252c0401899667447a8180593
Sam Clavette <samclavette@yahoo.com>
2020-08-02 17:36:18 -0500

    Initial commit: I added a Contestant class with member variables for first/last name, email address, and registration number. I also created a UserInterface class.

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private int registrationNumber;
+    }
+}
